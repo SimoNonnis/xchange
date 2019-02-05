d87d1c291ef6388771488c2f48b58868d8716d26
Simon Nonnis <simon.nonnis@protonmail.com>
2019-02-05 13:37:59 +0000

    :recycle: Refactor folder names

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+
 import store, { history } from './store';
-import App from './containers/app';
+import App from './components/app';
 
 import 'sanitize.css/sanitize.css';
 import './index.css';
