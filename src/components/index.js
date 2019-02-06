3a6ce304134f53ab2260f210f364470141e93186
Simon Nonnis <simon.nonnis@protonmail.com>
2019-02-06 16:38:45 +0000

    :sparkles: Set up interval

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Exchange from './Pages/Exchange';
 import Rates from './Pages/Rates';
 
-const App = () => (
-  <div className="app-container">
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/exchange" component={Exchange} />
-      <Route exact path="/rates" component={Rates} />
-    </main>
-  </div>
-);
+class App extends Component {
+  componentDidMount() {
+    console.log('getXchangeRates action');
+    this.getXchangeRatesInterval();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.getXchangeRatesInterval);
+  }
+
+  getXchangeRatesInterval = () =>
+    setInterval(() => console.log('getXchangeRates action interval'), 10000);
+
+  render() {
+    return (
+      <div className="app-container">
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/exchange" component={Exchange} />
+          <Route exact path="/rates" component={Rates} />
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
