6f8d38cd4cbc64d5c26a050c63ff5fd52f536071
Simon Nonnis <simon.nonnis@protonmail.com>
2019-02-14 15:56:15 +0000

    :recycle: getRates() only if state is empty

--- src/components/Pages/Exchange/index.js
+++ src/components/Pages/Exchange/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { isEmpty } from 'lodash';
 
 import {
   selectPocketsList,
@@ -53,7 +54,9 @@ class Exchange extends Component {
   };
 
   componentDidMount() {
-    this.props.getRates();
+    const { rates, getRates } = this.props;
+
+    if (isEmpty(rates)) getRates();
   }
 
   componentDidUpdate(prevProps) {
